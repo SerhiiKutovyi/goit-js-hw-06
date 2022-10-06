88d7d4fde16fcba058760b647e09c55c3ee78b59
SerhiiKutovyi <wagenrex@gmail.com>
2022-10-06 13:00:41 +0300

    JS-HW-6-end

--- js/task-08.js
+++ js/task-08.js
@@ -10,6 +10,7 @@ function onFormSubmit(event) {
   const formElements = event.currentTarget.elements;
   const mail = formElements.email.value;
   const password = formElements.password.value;
+
   const formData = { mail, password };
   console.log(formData);
   event.target.reset();
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
-function getRandomHexColor() {
+const refButton = document.querySelector('.change-color');
+
+const refSpan = document.querySelector('.color');
+
+refButton.addEventListener('click', getRandomHexColor);
+function getRandomHexColor(event) {
+  let randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  refSpan.style.backgroundColor = randomColor;
+  refSpan.textContent = randomColor;
+  document.body.style.backgroundColor = randomColor;
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
