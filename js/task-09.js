16f14daf0667695a326c2f4bb7d92b5c5cebc924
SerhiiKutovyi <wagenrex@gmail.com>
2022-10-06 14:56:09 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,10 +3,8 @@ const refButton = document.querySelector('.change-color');
 const refSpan = document.querySelector('.color');
 
 refButton.addEventListener('click', getRandomHexColor);
-function getRandomHexColor(event) {
-  let randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  refSpan.style.backgroundColor = randomColor;
+function getRandomHexColor() {
+  const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   refSpan.textContent = randomColor;
   document.body.style.backgroundColor = randomColor;
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
